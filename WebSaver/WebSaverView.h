465710e82a1af26ea03cacc34baadc84e29dccc8
Greg Kellogg <greg@kelloggs.fm>
2012-09-21 19:51:23 -0700

    Initial Commit

---
+++ WebSaver/WebSaverView.h
@@ -0,0 +1,13 @@
+//
+//  WebSaverView.h
+//  WebSaver
+//
+//  Created by Greg Kellogg on 9/21/12.
+//
+//
+
+#import <ScreenSaver/ScreenSaver.h>
+
+@interface WebSaverView : ScreenSaverView
+
+@end
---
+++ WebSaver/WebSaverView.m
@@ -0,0 +1,52 @@
+//
+//  WebSaverView.m
+//  WebSaver
+//
+//  Created by Greg Kellogg on 9/21/12.
+//
+//
+
+#import "WebSaverView.h"
+
+@implementation WebSaverView
+
+- (id)initWithFrame:(NSRect)frame isPreview:(BOOL)isPreview
+{
+    self = [super initWithFrame:frame isPreview:isPreview];
+    if (self) {
+        [self setAnimationTimeInterval:1/30.0];
+    }
+    return self;
+}
+
+- (void)startAnimation
+{
+    [super startAnimation];
+}
+
+- (void)stopAnimation
+{
+    [super stopAnimation];
+}
+
+- (void)drawRect:(NSRect)rect
+{
+    [super drawRect:rect];
+}
+
+- (void)animateOneFrame
+{
+    return;
+}
+
+- (BOOL)hasConfigureSheet
+{
+    return NO;
+}
+
+- (NSWindow*)configureSheet
+{
+    return nil;
+}
+
+@end
